b1d99b3c55b464627e3d958b5c4e17aaca7b0598
gst <shutonggao@163.com>
2017-03-24 14:07:26 +0800

    test1

--- src/Test.java
+++ src/Test.java
@@ -10,7 +10,7 @@ public class Test {
 		Thread.sleep(1000*15);
 		Date  t1=new Date();
 		System.out.println((t1.getTime()-t.getTime())/1000);
-		
+		System.out.println("1111111111111111111111111111");
 	} catch (InterruptedException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
